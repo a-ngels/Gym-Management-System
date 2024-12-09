600932c55b9716a0973d347e048a3bd9163add75
Morris Chang <mdc9136@nyu.edu>
2024-12-09 05:46:16 -0500

    added user class

---
+++ User.java
@@ -0,0 +1,39 @@
+public class User {
+
+    // fields
+    private static int totalUsers = 0;
+    private String first_name, last_name, phone_number, email;
+
+    public User(String first_name, String last_name, String phone_number, String email) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.phone_number = phone_number;
+        this.email = email;
+        totalUsers++;
+    }
+
+    // getters
+    public static int getNumUsers() {
+        return totalUsers;
+    }
+
+    public String getName() {
+        return first_name + " " + last_name;
+    }
+
+    public String getFirstName() {
+        return first_name;
+    }
+
+    public String getLastName() {
+        return last_name;
+    }
+
+    public String getPhoneNumber() {
+        return phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
